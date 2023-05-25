7509c9fe29ebd0db6e2efb68e8fd480d2c9e61d1
Chantal Goethals <chantalgoethals@gmail.com>
2023-05-25 17:26:48 -0600

    Feat: adjust media queries & convert btn flex direction to column

--- src/LandingPageOne/LandingPageOne.js
+++ src/LandingPageOne/LandingPageOne.js
@@ -1,40 +1,35 @@
 // import PropTypes from "prop-types"
-import React from 'react'
-import './LandingPageOne.css'
-import ReservationButton from '../controls/ReservationButton/ReservationButton'
-import Header from '../Header/Header'
-import '../Header/Header.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./LandingPageOne.css";
+import ReservationButton from "../controls/ReservationButton/ReservationButton";
+import Header from "../Header/Header";
+import "../Header/Header.css";
+import { Link } from "react-router-dom";
 
-
-const LandingPageOne = ({setGoToReservation}) => {
+const LandingPageOne = ({ setGoToReservation }) => {
   return (
     <div className="landing-page-one-container">
       <div className="landing-one-header">
-        <Header className="land-1-header"/>
+        <Header className="land-1-header" />
       </div>
       <section className="quote-one-container">
         <div className="quote-one-wrapper">
           <p className="galactic-quote">Your galactic journey awaits. . . </p>
         </div>
-      </section> 
+      </section>
       <div className="reservation-p1-button-container">
-      <Link  to={"/reservation-details"}>
-      <button className="back-to-home-res-pg" >
-        Reservations
-      </button>
-    </Link>
-         <Link to={"/reservations"} className='landing1-a'>
-           <ReservationButton className="reservation-button-landing1" input='Reserve' text='Reserve' onClick={(() => {setGoToReservation(1)})}/>
-            {/* <ReservationButton className="reservation-button" input='Reserve' /> */}
-         </Link>
+        <Link className="landing1-a" to={"/reservation-details"}>
+          <button className="back-to-home-btn-res-pg-l1 ">Reservations</button>
+        </Link>
+        <Link className="landing1-a" to={"/reservations"}>
+          <button className="back-to-home-btn-res-pg-l1 ">Reserve</button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default  LandingPageOne;
+export default LandingPageOne;
 
 // LandingPageOne.propTypes = {
 //   setGoToReservation: PropTypes.func
